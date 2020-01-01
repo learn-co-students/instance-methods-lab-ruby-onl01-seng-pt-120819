06b55e8e54c2602e4548b9ff7befe5a4228c9671
Ricky dildy <rdildy92@gmail.com>
2020-01-01 16:44:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+
+  def bark 
+    puts "Woof!"
+  end 
+fido = Dog.new 
+fido.bark
+def sit 
+  fido = Dog.new 
+  puts "The Dog is sitting"
+end 
+end 
+  
+
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  
+  def talk 
+    puts "Hello World!"
+  end 
+ ada  = Person.new 
+ ada.talk 
+ def walk 
+   ada = Person.new
+   puts "The Person is walking"
+ end 
+ end 
+   
